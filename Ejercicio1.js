43c6570271dd7ffa06cb55d234cc5fe42517a8cc
Aarón Gómez <nomeacuerdobien@gmail.com>
2025-02-14 20:26:44 +0100

    Create Ejercicio1.js

---
+++ Ejercicio1.js
@@ -0,0 +1,10 @@
+let miSuperHeroeFavorito = 'Hulk'
+let numeroFavorito = 50
+let altura = 5
+let longitud = 10
+
+let suma = altura + longitud
+
+console.log('Mi super héroe favorito es: ' + miSuperHeroeFavorito)
+console.log('Mi número favorito es: ' + numeroFavorito)
+console.log('la suma de la altura: ' + altura +' y la longitud: ' +longitud +' es: ' + suma)
